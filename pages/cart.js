4d2d0840ffbc97d774c211cbe9cc3ad7098945dd
Sulav <sulavgautam@proton.me>
2023-06-16 15:57:55 +0545

    final one

--- pages/cart.js
+++ pages/cart.js
@@ -4,12 +4,36 @@ import Link from "next/link";
 import Wrapper from "@/components/Wrapper";
 import CartItem from "@/components/CartItem";
 import { useSelector } from "react-redux";
+
+import { makePaymentRequest } from "@/utils/api";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+
 const Cart = () => {
+  const [loading, setLoading] = useState(false);
   const { cartItems } = useSelector((state) => state.cart);
 
   const subTotal = useMemo(() => {
     return cartItems.reduce((total, val) => total + val.attributes.price, 0);
   }, [cartItems]);
+
+  const handlePayment = async () => {
+    try {
+      setLoading(true);
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest("/api/orders", {
+        products: cartItems,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.stripeSession.id,
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
   return (
     <div className="w-full md:py-20">
@@ -23,6 +47,7 @@ const Cart = () => {
               </div>
             </div>
             {/* HEADING AND PARAGRAPH END */}
+
             {/* CART CONTENT START */}
             <div className="flex flex-col lg:flex-row gap-12 py-10">
               {/* CART ITEMS START */}
@@ -33,16 +58,18 @@ const Cart = () => {
                 ))}
               </div>
               {/* CART ITEMS END */}
-              {/* Summary start  */}
+
+              {/* SUMMARY START */}
               <div className="flex-[1]">
                 <div className="text-lg font-bold">Summary</div>
+
                 <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
                   <div className="flex justify-between">
                     <div className="uppercase text-md md:text-lg font-medium text-black">
                       Subtotal
                     </div>
                     <div className="text-md md:text-lg font-medium text-black">
-                      &#8360;{subTotal}
+                      &#8377;{subTotal}
                     </div>
                   </div>
                   <div className="text-sm md:text-md py-5 border-t mt-5">
@@ -52,16 +79,23 @@ const Cart = () => {
                     transaction fees.
                   </div>
                 </div>
+
                 {/* BUTTON START */}
-                <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center">
+                <button
+                  className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
+                  onClick={handlePayment}
+                >
                   Checkout
+                  {loading && <img src="/spinner.svg" />}
                 </button>
                 {/* BUTTON END */}
               </div>
-              {/* Summary end  */}
+              {/* SUMMARY END */}
             </div>
+            {/* CART CONTENT END */}
           </>
         )}
+
         {/* This is empty screen */}
         {cartItems.length < 1 && (
           <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
